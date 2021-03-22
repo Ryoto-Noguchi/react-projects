fd118ca6192f00212ea50532763a9a58ea748d5e
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-03-22 21:19:38 +0900

    1:52:07

--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -1,11 +1,23 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+console.log(allCategories);
 
 function App() {
-  const [menuItems, setMenuItems] = useState(items)
-  const [categories, setCategories] = useState([])
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
   return (
     <main>
       <section className="menu section">
@@ -13,12 +25,14 @@ function App() {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories></Categories>
+        <Categories
+          categories={categories}
+          filterItems={filterItems}
+        ></Categories>
         <Menu items={menuItems}></Menu>
       </section>
     </main>
-
-  )
+  );
 }
 
 export default App;
