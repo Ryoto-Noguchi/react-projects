546650a2fb1bd1845b66325dbc5e1c7f2887a2fb
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-04-04 17:18:29 +0900

    4:41

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -2,9 +2,19 @@ import React, { useState, useEffect } from "react";
 import List from "./List";
 import Alert from "./Alert";
 
+const getLocalStorage = () => {
+  // ユーザーの使っているブラウザにデータを保存する仕組み。ローカルで扱えるデータベースみたいなイメージ。keyとvalueのペアで保存される
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(localStorage.getItem("list"));
+  } else {
+    return [];
+  }
+};
+
 function App() {
   const [name, setName] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({
@@ -64,6 +74,10 @@ function App() {
     setEditID(id);
     setName(specificItem.title);
   };
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list)); // submitでitemが追加された状態のlistの中身をStringとして取得してくる。そしてそれを「list」というkeyにつけて保存する
+  }, [list]);
 
   return (
     <section className="section-center">
