736b3cfc00a8f61f110f96a376b4e4f3b75be1ee
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-04-11 21:01:40 +0900

    8:26

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="sectin about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam saepe explicabo laborum alias voluptatem velit veritatis adipisci delectus corporis? Eaque illum, aut sint molestias eligendi voluptatibus totam obcaecati tempore qui fugit quaerat suscipit consectetur at fugiat eveniet asperiores dolore veniam magni aliquam! Odio consequuntur quo inventore cumque expedita, exercitationem provident!</p>
+    </section>
   )
 }
 
